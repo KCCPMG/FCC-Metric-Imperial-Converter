45531d6dd70afde69341269ac8e94c8209f0a07c
Glitch (robust-kumquat) <none>
2019-09-29 02:13:39 +0000

    😚⛲️ Checkpoint ./tests/1_unit-tests.js:6206543/72

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -88,6 +88,8 @@ suite('Unit Tests', function(){
   suite('Function convertHandler.spellOutUnit(unit)', function() {
     
     test('For Each Valid Unit Inputs', function(done) {
+      var input = ['gal','l','mi','km','lbs','kg'];
+      var expect = 
       //see above example for hint
       // done();
     });
