6b95ae1f5f86330892c44400c8e43e07f84a94a6
Glitch (robust-kumquat) <none>
2019-09-29 03:47:39 +0000

    🐉🕌 Checkpoint ./tests/2_functional-tests.js:6206543/255

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -35,19 +35,19 @@ suite('Functional Tests', function() {
       
       test('Convert 32g (invalid input unit)', function(done) {
         chai.request(server)
-          .get('api/convert')
+          .get('/api/convert')
           .query({input: '32g'})
           .end(function(err, res) {
             assert.equal(res.status, 200);
-            assert.euqal(res.body.initNum, 32);
+            assert.equal(res.body.initNum, 32);
             assert.equal(res.body.initUnit, 'invalid unit');
-            assert.equal(res.body.returnUnit, null);
+            assert.equal(res.body.returnUnit, 'invalid unit');
+            done();
         });
-
       });
       
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
-        
+        chai.request('/api/')
         //done();
       });  
       
